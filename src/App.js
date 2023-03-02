51cdc22027970233b87756b751d2f979f474e041
unknown <michael.almandeel@gmail.com>
2023-03-01 22:47:04 -0500

    fianl commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/michaelalmandeel"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
